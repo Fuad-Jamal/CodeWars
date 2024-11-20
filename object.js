01d4491a22d5a72df57a9f73b654e0da2cbf10a1
Fuad Jamal <jamalfuad34@gmail.com>
2024-11-20 10:16:50 +0200

    object course

---
+++ object.js
@@ -0,0 +1,24 @@
+
+  let classRoom = [
+    {
+    net: 1,
+    id: 9,
+    grn: 'f',
+},
+
+{
+        get:2,
+        id:8,
+        grn: 'm',
+},
+{
+
+    bit:3,
+    id:9,
+    grn:'m'
+
+}
+
+]
+
+console.log(classRoom[0].id)   
