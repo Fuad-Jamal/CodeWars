9617c0700359eae4f31c5c75dd44ce8e4f0cc6e5
Fuad Jamal <jamalfuad34@gmail.com>
2024-11-21 12:03:42 +0200

    counting positives and adding negatives

---
+++ countPositiveAddNegative.js
@@ -0,0 +1,15 @@
+function countPositivesSumNegatives(input) {
+    if(input === null )return []
+  let positiveNumbers = 0
+  let negativeNumbers = 0
+for ( let i=0; i<input.length; i++){
+  if (input[i]>0){
+     positiveNumbers++
+  }else if(input[i]<0){
+  negativeNumbers += input[i]
+    
+  }
+}
+return input.length !==0? [positiveNumbers,negativeNumbers]:[]
+  }
+console.log(countPositivesSumNegatives([]))
