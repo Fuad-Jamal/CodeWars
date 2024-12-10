d07e5b3c6676b8a79fb6f667bad5f5a6209c8559
Fuad Jamal <jamalfuad34@gmail.com>
2024-12-10 12:07:42 +0200

    katas

--- countPositiveAddNegative.js
+++ countPositiveAddNegative.js
@@ -12,4 +12,4 @@ for ( let i=0; i<input.length; i++){
 }
 return input.length !==0? [positiveNumbers,negativeNumbers]:[]
   }
-console.log(countPositivesSumNegatives([]))
+console.log(countPositivesSumNegatives([7,-9,8,-9]))
