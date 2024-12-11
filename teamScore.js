9df1376d5f2783e09637c15a18ecf4488e2198f3
Fuad Jamal <jamalfuad34@gmail.com>
2024-12-11 11:49:21 +0200

    team scores

---
+++ teamScore.js
@@ -0,0 +1,12 @@
+function uefaEuro2016(teams, scores){
+    let [t1,t2] = teams
+  let [s1,s2] = scores
+  if (s1>s2){
+      return `At match ${t1} - ${t2}, ${t1} won!`
+  }else if(s1<s2){
+      return `At match ${t1} - ${t2}, ${t2} won!`
+  }else{
+      return `At match ${t1} - ${t2}, teams played draw.`
+  }
+  }
+  console.log(uefaEuro2016(['shaktar','marseille'],[3,1]))
