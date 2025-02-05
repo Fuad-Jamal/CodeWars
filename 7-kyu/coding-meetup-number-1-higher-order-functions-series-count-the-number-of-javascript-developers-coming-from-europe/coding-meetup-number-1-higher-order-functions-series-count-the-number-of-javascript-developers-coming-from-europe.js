1f9a2fbb9304778cc8b11d01ea5a4b1c94ae88b9
Gisa Fuad Jamal <jamalfuad34@gmail.com>
2025-02-05 09:28:05 +0200

    Add solution - CodeHub

---
+++ 7-kyu/coding-meetup-number-1-higher-order-functions-series-count-the-number-of-javascript-developers-coming-from-europe/coding-meetup-number-1-higher-order-functions-series-count-the-number-of-javascript-developers-coming-from-europe.js
@@ -0,0 +1,5 @@
+function countDevelopers(list) {
+  // your awesome code here :
+let ans = list.filter((developer)=> developer.continent=== 'Europe' && developer.language=== 'JavaScript').length;  return ans
+  return ans
+}
