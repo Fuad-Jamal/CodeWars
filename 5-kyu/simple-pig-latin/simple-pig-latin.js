a544e734baa61cfe54f503e67575762256bbc36f
Gisa Fuad Jamal <jamalfuad34@gmail.com>
2025-02-11 09:36:29 +0200

    Add solution - CodeHub

---
+++ 5-kyu/simple-pig-latin/simple-pig-latin.js
@@ -0,0 +1,12 @@
+ 
+function pigIt(str){
+  //Code here
+  let swich = str.split('')
+  let chnge = swich.map(
+  if(!isNaN(swich.charAt(0)) ){
+    return swich
+  }
+    return swich.slice(1) + word[0] + 'ay'.join('')
+  )
+​
+}
