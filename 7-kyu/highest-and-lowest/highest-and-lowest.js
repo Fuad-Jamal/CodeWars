bff7abd65c83b8df8aa795cbf4fb0efa9043c2df
Gisa Fuad Jamal <jamalfuad34@gmail.com>
2025-04-25 11:33:30 +0200

    Update solution - CodeHub

--- 7-kyu/highest-and-lowest/highest-and-lowest.js
+++ 7-kyu/highest-and-lowest/highest-and-lowest.js
@@ -2,7 +2,7 @@ function highAndLow(numbers){
   let y =''
   for(const x of numbers){
     if(x>x){
-      return y+= Math.min(x), Math.max(x)
+      return y+= Math.min(x) Math.max(x)
     }
     }
   return y
