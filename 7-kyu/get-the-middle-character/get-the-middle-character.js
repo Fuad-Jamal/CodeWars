ddd8598e3b61f9d7c0af758a60e085ea7f8997ab
Gisa Fuad Jamal <jamalfuad34@gmail.com>
2025-05-22 19:36:46 +0200

    Update solution - CodeHub

--- 7-kyu/get-the-middle-character/get-the-middle-character.js
+++ 7-kyu/get-the-middle-character/get-the-middle-character.js
@@ -3,7 +3,7 @@ function getMiddle(s) {
     let mid = Math.floor(s.length/2)
     let mid2 = s.charAt(mid)+ s.charAt(mid)
     let mid3 = s.charAt(mid2+1)
-    if(s.length%2 !== 0){
+    if(s.length%2 === 0){
    return mid2
     }else{
        return mid3
