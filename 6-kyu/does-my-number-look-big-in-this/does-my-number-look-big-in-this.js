d48858cde02617f14cd99a95c0a72fc67005fcee
Gisa Fuad Jamal <jamalfuad34@gmail.com>
2025-05-23 17:06:02 +0200

    Update solution - CodeHub

--- 6-kyu/does-my-number-look-big-in-this/does-my-number-look-big-in-this.js
+++ 6-kyu/does-my-number-look-big-in-this/does-my-number-look-big-in-this.js
@@ -1,4 +1,4 @@
-   function narcissistic(value) {
+function narcissistic(value) {
   let x = value.toString().length
   let y = value.toString().split('').map((xx)=>{
       let exp = Number(xx)
@@ -6,7 +6,5 @@
   })
  let sum = y.reduce((a,c)=> a+c,0)
  return sum === value? true: false
- 
- 
 }
 ​
