4c236df9407ae333cf5efd4c66023e34918b7e23
Gisa Fuad Jamal <jamalfuad34@gmail.com>
2025-07-02 13:01:12 +0200

    Update solution - CodeHub

--- 6-kyu/simple-max-digit-sum/simple-max-digit-sum.js
+++ 6-kyu/simple-max-digit-sum/simple-max-digit-sum.js
@@ -2,6 +2,7 @@
 function solve(n){
     if(n===0) return 0
   let x = solve(n-1)
+  let nss = n.toString().split('').reduce((a,b)=> a+ +b)
   let z = x.toString().split('').reduce((a,b)=>a+ +b,0)
-  return z
+  return nss<z? nss: z
 }
