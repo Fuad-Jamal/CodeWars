f6a4db3505498ff48ef53750caaa6ec5ef1bdaad
Gisa Fuad Jamal <jamalfuad34@gmail.com>
2025-07-23 18:07:00 +0200

    Add solution - CodeHub

---
+++ 5-kyu/convert-pascalcase-string-into-snake-case/convert-pascalcase-string-into-snake-case.js
@@ -0,0 +1,13 @@
+function toUnderscore(string) {
+  let result = ''
+    if(typeof string === 'number') return `${string}`
+    for(let x =0; x<string.length; x++{
+      
+      if(x!==0 && /[A-Z]/.test(string[i])){
+      result += '_'
+    }else{
+      result += string[i]
+    }
+    }
+    return result.toLowerCase()
+}
