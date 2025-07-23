85f5cd2a5794afe8d1c7b3a004c503d05388e545
Gisa Fuad Jamal <jamalfuad34@gmail.com>
2025-07-23 17:35:44 +0200

    Update solution - CodeHub

--- 6-kyu/string-array-duplicates/string-array-duplicates.js
+++ 6-kyu/string-array-duplicates/string-array-duplicates.js
@@ -1,13 +1,14 @@
  
 function dup(s) {
-    let result = ''
+ 
 let x = s.map(y=>{
-​
+   let result = ''
   for(let i=0; i<y.length; i++){
     if(y[i]!==y[i-1]){
       result += y[i]
     }
   }
+  return result
 })
-return result
+​
 };
