0de7445c649788ac85e4cd81650e6165376b16c2
Gisa Fuad Jamal <jamalfuad34@gmail.com>
2025-09-18 15:23:27 +0200

    Add solution - CodeHub

---
+++ 6-kyu/write-number-in-expanded-form/write-number-in-expanded-form.js
@@ -0,0 +1,12 @@
+function expandedForm(num) {
+  let str = String(num)
+  let soln = []
+  for(let i=0; i<str.length; i++){
+    let numb = parseInt(str[i])
+    if(numb!==0){
+      let exp = numb* Math.pow(10, str.length-1-i)
+      soln.push(exp)
+    }
+  }
+  return soln.join(' + ')
+}
