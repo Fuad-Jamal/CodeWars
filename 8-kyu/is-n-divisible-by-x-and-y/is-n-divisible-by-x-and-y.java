97ccf1d456e2d12b17cd2b64470fcc488d589f0e
Gisa Fuad Jamal <jamalfuad34@gmail.com>
2025-10-16 20:42:21 +0200

    Update solution - CodeHub

--- 8-kyu/is-n-divisible-by-x-and-y/is-n-divisible-by-x-and-y.java
+++ 8-kyu/is-n-divisible-by-x-and-y/is-n-divisible-by-x-and-y.java
@@ -1,6 +1,7 @@
+ 
 public class DivisibleNb {
   public static boolean isDivisible(long n, long x, long y) {
-    if(n/x==0 && n/y==0){
+    if(n%x==0 && n%y==0){
      return true;
     }else{
       return false;
