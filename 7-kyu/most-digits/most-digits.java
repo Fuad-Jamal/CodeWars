9ee9a8ed2f3b1b87539abedae4397d7a477b2969
Gisa Fuad Jamal <jamalfuad34@gmail.com>
2025-11-17 10:02:25 +0200

    Update solution - CodeHub

--- 7-kyu/most-digits/most-digits.java
+++ 7-kyu/most-digits/most-digits.java
@@ -1,11 +1,9 @@
 public class MostDigits {
   public static int findLongest(int[] numbers) {
     int result =0;
-    for(int i=0; i<numbers.length; i++){
-      for(int j=0; j<numbers.length; j++){
-        if(Math.max(numbers[i])==Math.max(numbers[j])) {
-          result +=numbers[i];
-          }
+    for(int i=0 i<numbers.length; i++){
+      if(result<numbers[i]){
+        result = numbers[i];
       }
     }
       return result;
