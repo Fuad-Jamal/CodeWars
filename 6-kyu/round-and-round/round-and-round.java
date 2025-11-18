0a718ca20f9cd292f59b027e39ef1ee2700eaedd
Gisa Fuad Jamal <jamalfuad34@gmail.com>
2025-11-18 18:50:10 +0200

    Add solution - CodeHub

---
+++ 6-kyu/round-and-round/round-and-round.java
@@ -0,0 +1,8 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+​
+public class RoundAndRound {
+    public static double roundTo2DecimalPlaces(double number) {
+        return BigDecimal.valueOf(number).setScale(2, RoundingMode.HALF_UP).doubleValue();
+    }
+}
